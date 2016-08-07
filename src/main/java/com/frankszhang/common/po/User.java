7dbb39dc9619e68f18ffee1ea1bc8899093623bf
Frank <asdj4030@gmail.com>
2016-08-07 12:18:09 +0800

    first commit

---
+++ src/main/java/com/frankszhang/common/po/User.java
@@ -0,0 +1,35 @@
+package com.frankszhang.common.po;
+
+/**
+ * @Description  用户类
+ * @author zwf<373561032@qq.com>
+ * @Date 2016年5月3日 下午7:14:56
+ * @version V1.0
+ */
+public class User {
+
+	private String id;
+	private String name;
+	private String password;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
