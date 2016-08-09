8cfa5c2039526327975f22fc6b6e8890d55798d4
Frank <asdj4030@gmail.com>
2016-08-09 21:56:56 +0800

    修改xml

--- src/main/java/com/frankszhang/web/UserController.java
+++ src/main/java/com/frankszhang/web/UserController.java
@@ -3,19 +3,21 @@ package com.frankszhang.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.frankszhang.common.po.User;
 import com.frankszhang.service.UserService;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
 
-	@Autowired
 	private UserService userServiceImpl;
 	
 	@RequestMapping("/addUser")
+	@ResponseBody
 	public String addUser(User user) {
 		
-		return "";
+		return "success";
 	}
 }
