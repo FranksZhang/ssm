effcd63600f96ad210bcecf6ba7c37ebe65d116a
Frank <asdj4030@gmail.com>
2017-01-11 10:55:40 +0800

    add log

--- src/main/java/com/frankszhang/web/UserController.java
+++ src/main/java/com/frankszhang/web/UserController.java
@@ -2,6 +2,8 @@ package com.frankszhang.web;
 
 import javax.annotation.Resource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -18,10 +20,20 @@ public class UserController {
 	@Resource
 	private UserMapper userMapper;
 	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
 	@RequestMapping("/addUser")
 	public String addUser(User user) {
 		int result = userService.insert(user);
 		System.out.println("result=" + result);
+		return "success";
+	}
+	
+	@RequestMapping("/testLogger")
+	public String testLogger() {
+		if(logger.isDebugEnabled()) {
+			logger.debug("I think it's ok!");
+		}
 		return "success";
 	}
 	
